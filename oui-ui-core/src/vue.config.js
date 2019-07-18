43563f8cfc53ee46a891d7f1898990622fd352db
luye <luye@cnsofeware.cn>
2019-07-18 15:26:28 +0800

    init

---
+++ oui-ui-core/src/vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  indexPath: 'oui.html',
+  productionSourceMap: false,
+  devServer: {
+    proxy: {
+      '/ubus': {
+        target: 'http://192.168.1.1'
+      },
+      '/cgi-bin/': {
+        target: 'http://192.168.1.1'
+      }
+    }
+  },
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}
